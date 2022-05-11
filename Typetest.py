2a8a930e4690e8bda10e53d43001863ac1bdabf5
Ananza008z <anansit1111@gmail.com>
2022-05-11 20:45:46 +0700

    Update Typetest.py

--- Typetest.py
+++ Typetest.py
@@ -1,3 +1,5 @@
+import time
+
 lst = []
 lst2 = []
 scr = []
@@ -9,6 +11,7 @@ for _ in range(int(input())):
     lst2.append(score)
     lst.append(lst2)
     lst2 = []
+start = time.time()
 for i in range(len(lst)):
     scr.append((lst[i][1]))
 for k in range(scr.count(min(scr))):
@@ -17,3 +20,7 @@ lst.sort()
 for j in range(len(lst)):
     if str(min(scr)) == str(lst[j][1]):
         print(lst[j][0])
+
+stop = time.time()
+
+print(stop-start)
